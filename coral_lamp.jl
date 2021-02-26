6f9ca9c0f32b267cccc981789eda571a16f2d8af
Johannes Fischer <johannes@fischer-guidon.de>
2021-02-27 00:52:46 +0100

    Further refactoring

--- coral_lamp.jl
+++ coral_lamp.jl
@@ -16,22 +16,22 @@ coral = Coral(v, radius)
 
 # ### TEST - compare pythagoras error on ball surface, i.e. these values should not be equal
 # # Curved lines
-# angle_side_tip = acos(dot(v[2], v[1]))
+# angle_side_tip = acos(dot(coral.side_a, coral.tip))
 # l_side_tip = R * angle_side_tip
 # p,q = l_side_tip, sqrt(l_side^2 + l_tip^2)
 # @show p,q
 # # Flat lines
-# p,q = norm(v[1]-v[2]), sqrt(norm(v[1]-v[6])^2 + norm(v[6]-v[2])^2)
+# p,q = norm(coral.tip-coral.side_a), sqrt(norm(coral.tip-coral.center)^2 + norm(coral.center-coral.side_a)^2)
 # @show p,q
 # p,q = norm(unnormalized[1]-unnormalized[2]), sqrt(norm(unnormalized[1]-unnormalized[6])^2 + norm(unnormalized[6]-unnormalized[2])^2)
 # @show p,q
 # ###
 
-@show sphere_angle(v[1],coral.center,v[2]) * 180 / pi
-@show sphere_angle(v[5],coral.center,v[2]) * 180 / pi
-@show sphere_angle(v[5],coral.center,v[1]) * 180 / pi
-@show sphere_angle(v[2],coral.center,v[3]) * 180 / pi
-@show sphere_angle(v[3],coral.center,v[4]) * 180 / pi
-@show sphere_angle(v[4],coral.center,v[5]) * 180 / pi
+@show sphere_angle(coral.tip,coral.center,coral.side_a) * 180 / pi
+@show sphere_angle(coral.side_b,coral.center,coral.side_a) * 180 / pi
+@show sphere_angle(coral.side_b,coral.center,coral.tip) * 180 / pi
+@show sphere_angle(coral.side_a,coral.center,coral.bottom_a) * 180 / pi
+@show sphere_angle(coral.bottom_a,coral.center,coral.bottom_b) * 180 / pi
+@show sphere_angle(coral.bottom_b,coral.center,coral.side_b) * 180 / pi
 
 sphere_surface_area = 4 * pi * radius^2
