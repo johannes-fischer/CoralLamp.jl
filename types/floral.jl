7dec0fcc5b82c6815ce2b613da8ccc13adc9dc5d
Johannes Fischer <johannes@fischer-guidon.de>
2021-03-17 23:39:38 +0100

    Add comment for floral

--- types/floral.jl
+++ types/floral.jl
@@ -1,1 +1,7 @@
 struct Floral end
+
+
+    # For corner circles in floral
+    # https://juliagraphics.github.io/Luxor.jl/stable/simplegraphics/#Circles-and-tangents
+    # line intersections https://juliagraphics.github.io/Luxor.jl/stable/geometrytools/#Intersections
+    
