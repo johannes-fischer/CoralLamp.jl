54bab1116061dc7fb12ad2c3ed637b6026dc7c4c
Johannes Fischer <johannes@fischer-guidon.de>
2021-03-26 22:21:44 +0100

    Start computing floral

--- types/floral.jl
+++ types/floral.jl
@@ -1,4 +1,29 @@
-struct Floral end
+using AngleBetweenVectors
+using Luxor
+using Rotations
+# using Luxor: offsetlinesegment
+# using IntegralArrays
+
+struct Floral3d
+    tip
+    side_a
+    side_b
+    bottom_a
+    bottom_b
+    center
+    radius
+end
+
+function Floral3d(data::PolyhedraTile, radius::Float64, stem_factor::Float64=1.0)
+    stem = stem_factor * radius / 15  # length 2cm looked good for 60cm diameter -> divide radius by 15
+    stem_angle = stem / radius
+    axis = cross(data.tip, data.bottom_a + data.bottom_b)  # rotation axis as cross product of tip and center between two bottom (will be normalized)
+    rot = AngleAxis(stem_angle, axis...)
+    center = rot * data.tip
+    points = [data.tip, data.side_a, data.side_b, data.bottom_a, data.bottom_b]
+    push!(points, center)
+    Floral3d(normalize.(points)..., radius)
+end
 
 
     # For corner circles in floral
--- util/spherical_geometry.jl
+++ util/spherical_geometry.jl
@@ -1,4 +1,6 @@
 using AngleBetweenVectors
+using StaticArrays
+using LinearAlgebra
 
 function sphere_angle(a,b,c,center=[0.0,0.0,0.0])
     # Compute spherical angle at b between arc AB and BC, relative to center
@@ -6,3 +8,34 @@ function sphere_angle(a,b,c,center=[0.0,0.0,0.0])
     n2 = cross(b - center, c - center)
     angle(n1, n2)
 end
+
+"""
+    3d plane defined by all points x with dot(normal, x) == distance
+"""
+struct Plane
+    normal::SVector{3}
+    distance::Real
+    Plane(n::SVector{3}, d::Real) = new(normalize(n), d)
+end
+Plane(normal::SVector{3}, p::SVector{3}) = Plane(normal, dot(normalize(normal), p))
+Plane(p1::SVector{3}, p2::SVector{3}, p3::SVector{3}) = Plane(cross(p2-p1, p3-p1), p1)
+
+"""
+    Circle with radius r on a sphere with radius R. Sphere center is the origin, circle center is given by center
+    (Note that the center vector is normal to the circle (plane in which it lies))
+"""
+struct SphereCircle
+    R  # sphere radius
+    # sphere center at origin
+    r  # circle radius
+    center  # 3d coordinates of circle center = normal of plane that intersects with sphere to produce circle
+end
+
+function sphereplaneintersection(R::Float64, plane::Plane)::SphereCircle
+    @assert abs(plane.distance) <= R
+    r = sqrt(R^2 - plane.distance^2)
+    center = plane.distance * plane.normal
+    SphereCircle(R, r, center)
+end
+
+# get tangent -> tangent is in plane -> intersection
