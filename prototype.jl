5b7d9fd22f6b2da15ccfa42623a1cca230a55641
Johannes Fischer <johannes@fischer-guidon.de>
2021-03-26 22:21:58 +0100

    cleanup prototype

--- prototype.jl
+++ prototype.jl
@@ -7,74 +7,6 @@ using InteractiveUtils
 # ╔═╡ 24775210-7a09-11eb-34b3-9175ee4fd488
 using Luxor, PlutoUI, Colors
 
-# ╔═╡ 33aa9544-7a09-11eb-05ef-9da3db67eb36
-@draw begin
-    x = ngon(O, 100, 4, 0, vertices=true)
-	line(O, x[1], :path)
-	line(O)
-	for i in 2:3
-		line(x[i])
-		line(O)
-	end
-	line(x[4])
-	closepath()
-	do_action(:stroke)
-end
-
-# ╔═╡ 49d7de80-7a09-11eb-139c-6b17efed44a4
-begin
-	de = Drawing(800,800, "test.svg")
-	origin()
-	#circle(O,100,:stroke)
-	newpath()
-	line(100,100)
-	line(-100,100)
-	rline(00,-100)
-	closepath()
-	line(Point(-100,0),Point(-200,-200), :path)
-	rline(200,0)
-	#closepath()
-	do_action(:stroke)
-	finish()
-	de
-end
-
-# ╔═╡ e750072c-7a0c-11eb-2b90-f5c10b7d1de2
-@draw begin
-	#x = ngon(O, 100, 4, 0, vertices=true)
-	points = Array{Point,1}()
-	for i in 1:4
-		append!(points, [O,x[i]])
-	end
-	poly(points, :stroke, close=true)
-	poly(offsetpoly(points, 30), :stroke, close=true)
-	p = ngon(O, 100, 3, 0)
-	#poly(p, :stroke, close=true)
-	sethue("red")
-	#poly(offsetpoly(p, 30), :stroke, close=true)
-end
-
-# ╔═╡ 6a92c0ba-85ce-11eb-0733-efc5f7ac0721
-function parallel_line(pt1, pt2, offset=0)
-	normal = perpendicular(pt2-pt1)
-	normal = normal / distance(normal, O) * offset
-	return [pt1 + normal, pt2 + normal]
-end
-
-# ╔═╡ e7e63e3e-838a-11eb-1a0c-39d2b41bef41
-@draw begin
-	pt = ngon(Point(0, 0), 80, 3, vertices=true)
-	#pt = [O,Point(100,0)]
-	poly(pt, :stroke)
-	#translate(200, 0)
-	#polysmooth(pt, 10, :stroke, debug=true)
-	sethue("red")
-	poly([p for p in Luxor.offsetlinesegment(pt..., 40, -40)], :stroke)
-	#poly(offsetpoly(pt), :stroke)
-	sethue("blue")
-	poly(parallel_line(pt[1:2]..., 20), :stroke)
-end
-
 # ╔═╡ 0dc0cffa-85d1-11eb-108e-995b8b449c62
 function cornersmooth(pt1, pt2, pt3, corner_radius)
 	_, center, _ = Luxor.offsetlinesegment(pt1, pt2, pt3, corner_radius, corner_radius)
@@ -181,14 +113,14 @@ import Luxor.offsetlinesegment
 	end
 end
 
+# ╔═╡ bac005ea-8ce0-11eb-002b-0f61fcba8355
+5/2
+
+
 # ╔═╡ Cell order:
 # ╠═24775210-7a09-11eb-34b3-9175ee4fd488
-# ╠═33aa9544-7a09-11eb-05ef-9da3db67eb36
-# ╠═49d7de80-7a09-11eb-139c-6b17efed44a4
-# ╠═e750072c-7a0c-11eb-2b90-f5c10b7d1de2
 # ╠═2537384c-8386-11eb-283c-b98a0971c685
-# ╠═e7e63e3e-838a-11eb-1a0c-39d2b41bef41
-# ╠═6a92c0ba-85ce-11eb-0733-efc5f7ac0721
 # ╠═0dc0cffa-85d1-11eb-108e-995b8b449c62
 # ╠═3832a56e-85d7-11eb-0e36-5d035ae3e079
 # ╠═405f061a-85d7-11eb-1249-e55633407b36
+# ╠═bac005ea-8ce0-11eb-002b-0f61fcba8355
