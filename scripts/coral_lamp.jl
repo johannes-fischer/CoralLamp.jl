ad611a33cc714d27682ec646af654ee2b5442152
Johannes Fischer <johannes@fischer-guidon.de>
2021-04-06 22:48:08 +0200

    Change hole diameter

--- scripts/coral_lamp.jl
+++ scripts/coral_lamp.jl
@@ -9,7 +9,7 @@ radius = diameter / 2
 
 coral = Coral2d(Coral3d(t, radius))
 width = diameter / 30
-hole_diameter = 5mm
+hole_diameter = 5.5mm
 
 print(coral)
 
--- scripts/floral_lamp.jl
+++ scripts/floral_lamp.jl
@@ -8,8 +8,8 @@ diameter = 60cm
 radius = diameter / 2
 
 floral = Floral2d(Floral3d(t, radius))
-width = diameter / 30
-hole_diameter = 5mm
+width = diameter / 40 # 1.5cm for 60cm diameter
+hole_diameter = 5.5mm
 
 print(floral)
 
